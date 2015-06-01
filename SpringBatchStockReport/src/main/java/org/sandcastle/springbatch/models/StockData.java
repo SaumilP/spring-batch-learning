933911dd5660c553d57c0d4cfcdabf5479504824
Saumil Patel <saumilpatel1@gmail.com>
2015-06-01 22:57:53 +0200

    POI Example - Initial Commit

---
+++ SpringBatchStockReport/src/main/java/org/sandcastle/springbatch/models/StockData.java
@@ -0,0 +1,8 @@
+package org.sandcastle.springbatch.models;
+
+/**
+ * Created by PATEL1 on 01/06/2015.
+ */
+public class StockData {
+
+}
---
+++ SpringBatchStockReport/src/main/java/org/sandcastle/springbatch/support/StockDataExcelWriter.java
@@ -0,0 +1,8 @@
+package org.sandcastle.springbatch.support;
+
+/**
+ * Created by PATEL1 on 01/06/2015.
+ */
+public class StockDataExcelWriter {
+
+}
